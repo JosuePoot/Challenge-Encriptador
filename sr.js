0183edcd6aba58e0efc96b6ba08baeef94cafae4
josue_poot <josuepootpool@gmail.com>
2024-02-04 18:45:05 -0600

    Eliminacion de archivos de prueba

--- sr.js
+++
@@ -1,7 +0,0 @@
-function copiarUrl(){
-    let url = document.getElementById("url").textContent;
-    let button = document.getElementById("botonCopiar");
-
-    navigator.clipboard.writeText(url);
-    button.textContent = "copiado";
-};
