cfa79b1c5057659ba1d4b6203b03b3921a1be8de
josue_poot <josuepootpool@gmail.com>
2024-02-11 14:37:30 -0600

    punto de guardado

--- script.js
+++ script.js
@@ -1,6 +1,48 @@
 let mensaje;
 let mensajeResultante;
 
+
+function limpiarTexto() {
+    var texto = document.getElementById("texto").value;
+
+    // Verificar si se ingresaron caracteres no permitidos antes de limpiar el texto
+    if (/[^a-z]/g.test(texto)) {
+        mostrarMensajeCaracterNoPermitido();
+        // Eliminar caracteres no permitidos y reasignar el valor al textarea
+        //texto = texto.replace(/[^a-záéíóú\s]/gi, '');
+        //document.getElementById("texto").value = texto;
+        //return;
+    }
+
+    // Normalizar caracteres y reemplazar caracteres acentuados
+    //texto = texto.normalize("NFD").replace(/[\u0300-\u036f]/g, '');
+
+    // Convertir todo a minúsculas
+    //texto = texto.toLowerCase();
+
+    document.getElementById("texto").value = texto;
+}
+
+function limpiarTexto2() {
+    var texto = document.getElementById("texto").value;
+    // Normalizar caracteres y reemplazar caracteres acentuados
+    texto = texto.normalize("NFD").replace(/[\u0300-\u036f]/g, '');
+    // Convertir todo a minúsculas
+    texto = texto.toLowerCase();
+    // Eliminar caracteres especiales
+    var textoLimpiado = texto.replace(/[^a-z\s]/g, '');
+    if (texto !== textoLimpiado) {
+        mostrarMensajeCaracterNoPermitido();
+    }
+
+    document.getElementById("texto").value = textoLimpiado;
+}
+
+function mostrarMensajeCaracterNoPermitido() {
+    var advertencia = document.querySelector("#advertencia");
+    advertencia.setAttribute('style','color: red;');
+}
+
 function encriptar(){
     estadosIniciales();
     mensaje = document.getElementById('texto').value;
